46edba315cf859ba22ccf6cbe4c9d2b2ed7053be
Thong Kuah <kuahyeow@gmail.com>
2009-08-30 16:00:39 +1200

    Make the homepage point to the only page we have

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "dataset_entities"
 
   # See how all your routes lay out with "rake routes"
 
