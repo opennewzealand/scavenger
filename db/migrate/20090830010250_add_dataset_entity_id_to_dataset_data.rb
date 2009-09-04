faabb16c04a301500d4739dcf4e5b4c874495976
Glen Barnes <barnaclebarnes@Glen-Barness-iMac.local>
2009-09-04 14:11:44 +1200

    Removed migratipon file which no longer makes sense

--- db/migrate/20090830010250_add_dataset_entity_id_to_dataset_data.rb
+++
@@ -1,9 +0,0 @@
-class AddDatasetEntityIdToDatasetData < ActiveRecord::Migration
-  def self.up
-    add_column :dataset_datas, :dataset_entity_id, :integer
-  end
-
-  def self.down
-    remove_column :dataset_datas, :dataset_entity_id
-  end
-end
