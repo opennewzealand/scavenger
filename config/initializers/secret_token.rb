6ae5e7ae56c3366bef20b969fc497756c3a442a5
Glen Barnes <glen@open.org.nz>
2010-08-23 15:30:51 +1200

    Upgraded code to Rails 3.0.rc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scavenger::Application.config.secret_token = '5a67e597b090aa0778b006d6b36aef27a8dc8b941d4d685ba99b183ab98bb5bfadab708f8dd1a27fb13924e854ae346af5ce6d694721ed501749809dbeab7a29'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_a_session',
-  :secret      => 'f3f377257417402cab4db6ebc0fb4f9112f56accf319892a6b7e6eb831a032ec85cf9ff63e807780549eed533f9a076e7fa8f7f99dae9eafceb8379ba9250c3b'
-}
+Scavenger::Application.config.session_store :cookie_store, :key => '_scavenger_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Scavenger::Application.config.session_store :active_record_store
